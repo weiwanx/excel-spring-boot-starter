e6ab21908e9267aa5623c446f48d25954b5f2fce
weiwanxi <weiwanx@163.com>
2021-04-23 17:31:27 +0800

    初始化项目

---
+++ src/main/java/com/excel/operation/ExcelOperationApplication.java
@@ -0,0 +1,27 @@
+package com.excel.operation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 批批网
+ * ================================================
+ * 版权所有: 广州市批来批往信息科技有限公司，并保留所有权利。
+ * 网站地址: https://www.ppwang.com
+ * ================================================
+ * ------------------------------------------------
+ * ExcelOperationApplication
+ *
+ * @author 万喜 <weiwanx@163.com>
+ * @description 描述内容
+ * @date 2021-04-23
+ * ------------------------------------------------
+ */
+@SpringBootApplication
+public class ExcelOperationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExcelOperationApplication.class, args);
+    }
+
+}
